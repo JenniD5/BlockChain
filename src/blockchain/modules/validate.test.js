0d4022a6cdbd49d382f90606de4096dbac1410f8
Your Name <you@example.com>
2022-09-21 17:27:39 -0700

    ..

--- src/blockchain/modules/validate.test.js
+++ src/blockchain/modules/validate.test.js
@@ -1,50 +1,44 @@
-import Blockchain from '../blockchain';
-import validate from './validate';
+import Blockchain from "../blockchain";
+import validate from "./validate";
 
-describe ('validate()', () =>{
+describe('validate()', () => {
     let blockchain;
 
-    beforeEach(()=>{
+    beforeEach(() => {
         blockchain = new Blockchain();
-
     });
 
-    it ('Crear cadena valida',()=>{
+    it('Crear cadena valida', () => {
         blockchain.addBlock('transact0');
         blockchain.addBlock('transact1');
 
         expect(validate(blockchain.blocks)).toBe(true);
     });
-    it ('Invalidando cadena con un genesis block corrupto',() =>{
+
+    it('Invalidando cadena con un genesis block corrupto', () => {
         blockchain.blocks[0].data = 'h4ck-data';
+
         expect(() => {
             validate(blockchain.blocks);
         }).toThrowError('Bloque Genesis Invalido');
     });
 
-    it ('Invalidando una cadena con un previousHash corrupto en un block', ()=>{
+    it('Invalidando una cadena con un previousHash corrupto en un block', () => {
         blockchain.addBlock('transact2');
-        blockchain.blocks[1].previousHash = 'h4ck-previousHash';
+        blockchain.blocks[1].previousHash = 'h4sh-previousHash';
 
         expect(() => {
             validate(blockchain.blocks);
-        }).toThrowError('Hash previo invalido');
+        }).toThrowError('Hash previo invalido o corrupto');
     });
 
-    it ('Invalidando una cadena con un block con hash corrupto', ()=>{
+    it('Invalidando una cadena con un block con hash corrupto', () => {
         blockchain.addBlock('transact3');
-        blockchain.blocks[1].hash = 'h4ck-hash';
+        blockchain.blocks[1].hash = 'h4sh-hash';
 
-        expect(()=>{
+        expect(() => {
             validate(blockchain.blocks);
+
         }).toThrowError('Hash Invalido');
     });
-
-   
-
-    
-
-   
-
-   
 });
